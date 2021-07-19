25e4a0877384b88bb0d1b7d3c6181e391bb7e6b4
Stav Gayer <stav.g@heilasystems.com>
2021-07-19 15:36:42 +0300

    monitoring dependency

---
+++ wrapper/builder.go
@@ -0,0 +1,80 @@
+package wrapper
+
+import (
+	monitor "bitbucket.org/HeilaSystems/monitoring"
+	"log"
+	"container/list"
+)
+
+type monitorConfig struct {
+	tags       monitor.Tags
+	extractors []monitor.ContextExtractor
+	onError    func(err error)
+	reporter   monitor.BricksReporter
+}
+
+// WrapperBuilder is a helper builder to define internal Mortar monitoring wrapper
+type WrapperBuilder interface {
+	// Build builds monitor.Reporter
+	Build(bricksBuilder monitor.Builder) monitor.Reporter
+	// DoOnError is a helper function to act when receiving an error during Metric creation
+	DoOnError(onError func(error)) WrapperBuilder
+	// AddExtractors adds ContextExtractors that might override tag values when calling metric functions
+	AddExtractors(extractors ...monitor.ContextExtractor) WrapperBuilder
+	// SetTags saves defaults tags, these tags will always be included in every metric
+	SetTags(tags monitor.Tags) WrapperBuilder
+}
+
+type wrapperBuilder struct {
+	ll *list.List
+}
+
+// Builder creates a WrapperBuilder
+func Builder() WrapperBuilder {
+	return &wrapperBuilder{
+		ll: list.New(),
+	}
+}
+
+func (b *wrapperBuilder) SetTags(tags monitor.Tags) WrapperBuilder {
+	b.ll.PushBack(func(cfg *monitorConfig) {
+		if tags != nil {
+			cfg.tags = tags // make sure tags are always empty, not nil
+		}
+	})
+	return b
+}
+
+func (b *wrapperBuilder) AddExtractors(extractors ...monitor.ContextExtractor) WrapperBuilder {
+	b.ll.PushBack(func(cfg *monitorConfig) {
+		cfg.extractors = append(cfg.extractors, extractors...)
+	})
+	return b
+}
+
+func (b *wrapperBuilder) DoOnError(onError func(error)) WrapperBuilder {
+	b.ll.PushBack(func(cfg *monitorConfig) {
+		cfg.onError = onError
+	})
+	return b
+}
+
+func (b *wrapperBuilder) Build(bricksBuilder monitor.Builder) monitor.Reporter {
+	cfg := new(monitorConfig)
+	for e := b.ll.Front(); e != nil; e = e.Next() {
+		f := e.Value.(func(*monitorConfig))
+		f(cfg)
+	}
+	if cfg.onError == nil {
+		cfg.onError = func(err error) {
+			log.Printf("WARNING: monitoring error, %v", err)
+		}
+	}
+	if cfg.tags == nil {
+		cfg.tags = monitor.Tags{}
+	}
+	cfg.reporter = bricksBuilder.Build()
+	return newMortarReporter(cfg)
+}
+
+var _ WrapperBuilder = (*wrapperBuilder)(nil)
---
+++ wrapper/reporter.go
@@ -0,0 +1,68 @@
+package wrapper
+
+import (
+	monitor "bitbucket.org/HeilaSystems/monitoring"
+	"context"
+)
+
+type mortarReporter struct {
+	externalMetrics monitor.BricksMetrics
+	cfg             *monitorConfig
+	registry        *externalRegistry
+}
+
+// NewMortarReporter creates a new mortar monitoring reporter which is a wrapper to support
+// 	- ContextExtractors
+// 	- Default Tags, for example: {"version":"v1.0.1", "service":"awesome"}
+//
+// Meaning, it is possible to also extract tag values from the context, this is useful when the value is set per request/call within the context.Context:
+// 	- Canary release https://martinfowler.com/bliki/CanaryRelease.html identifier
+// 	- Authentication Token values, but avoid using high cardinality values such as UserID
+//
+func newMortarReporter(cfg *monitorConfig) monitor.Reporter {
+	externalMetrics := cfg.reporter.Metrics()
+	return &mortarReporter{
+		externalMetrics: externalMetrics,
+		cfg:             cfg,
+		registry:        newRegistry(externalMetrics),
+	}
+}
+
+func (r *mortarReporter) Connect(ctx context.Context) error {
+	return r.cfg.reporter.Connect(ctx)
+}
+
+func (r *mortarReporter) Close(ctx context.Context) error {
+	return r.cfg.reporter.Close(ctx)
+}
+
+func (r *mortarReporter) Metrics() monitor.Metrics {
+	return r
+}
+
+// Counter creates a counter with possible predefined tags
+func (r *mortarReporter) Counter(name string, desc string) monitor.TagsAwareCounter {
+	return newMetric(r.registry, r.cfg).WithTags(r.cfg.tags).Counter(name, desc)
+}
+
+// Gauge creates a gauge with possible predefined tags
+func (r *mortarReporter) Gauge(name string, desc string) monitor.TagsAwareGauge {
+	return newMetric(r.registry, r.cfg).WithTags(r.cfg.tags).Gauge(name, desc)
+}
+
+// Histogram creates a histogram with possible predefined tags
+func (r *mortarReporter) Histogram(name string, desc string, buckets monitor.Buckets) monitor.TagsAwareHistogram {
+	return newMetric(r.registry, r.cfg).WithTags(r.cfg.tags).Histogram(name, desc, buckets)
+}
+
+// Timer creates a timer with possible predefined tags
+func (r *mortarReporter) Timer(name string, desc string) monitor.TagsAwareTimer {
+	return newMetric(r.registry, r.cfg).WithTags(r.cfg.tags).Timer(name, desc)
+}
+
+// WithTags sets custom tags to be included if possible in every Metric
+func (r *mortarReporter) WithTags(tags monitor.Tags) monitor.Metrics {
+	return newMetric(r.registry, r.cfg).
+		WithTags(r.cfg.tags). // first apply default tags
+		WithTags(tags)        // then apply custom ones
+}
